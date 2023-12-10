fc9f056fe72a01870fe044558851c3750d419040
Shriprakash Tiwari <sptiwari46289@gmail.com>
2023-12-10 16:01:32 +0530

    minor fix

--- connection.py
+++ connection.py
@@ -6,7 +6,7 @@ from tools import read_from_file
 
 def createEngine():
     config = json.loads(read_from_file("dbconfig.json"))
-    database_uri = f"postgresql+psycopg2://{config['username']}:{{config['password']}}@{{config['host']}}:{{config['port']}}/{{config['dbname']}}"
+    database_uri = f"postgresql+psycopg2://{config['username']}:{config['password']}@{config['host']}:{config['port']}/{config['dbname']}"
     return create_engine(database_uri, echo=False)
 
 
