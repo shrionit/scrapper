32652534b6da9a4abd3229dbec102adccfa601fe
Shriprakash Tiwari <sptiwari46289@gmail.com>
2023-12-20 18:52:21 +0530

    minor changes

--- connection.py
+++ connection.py
@@ -7,7 +7,7 @@ def createEngine():
     configpath = "dbconfig.json"
     config = json.loads(read_from_file(configpath))
     database_uri = f"postgresql+psycopg2://{config['username']}:{config['password']}@{config['host']}:{config['port']}/{config['dbname']}"
-    return create_engine(database_uri, echo=False)
+    return create_engine(database_uri, pool_size=30, max_overflow=10, echo=False)
 
 
 engine = createEngine()
